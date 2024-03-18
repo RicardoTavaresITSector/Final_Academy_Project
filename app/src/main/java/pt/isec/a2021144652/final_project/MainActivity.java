8ea28dbf9b368158d832332bf3034b42df219908
raatavares <163897599+RicardoTavaresITSector@users.noreply.github.com>
2024-03-18 19:56:52 +0000

    Initial project setup

---
+++ app/src/main/java/pt/isec/a2021144652/final_project/MainActivity.java
@@ -0,0 +1,14 @@
+package pt.isec.a2021144652.final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
