a588f72701709a23c68252296f9ce415598352a0
RicardoTavaresITSector <163897599+RicardoTavaresITSector@users.noreply.github.com>
2024-03-26 11:21:37 +0000

    Add internet connection verification

--- app/src/main/java/pt/isec/a2021144652/final_project/activities/MainActivity.java
+++ app/src/main/java/pt/isec/a2021144652/final_project/activities/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,13 +35,41 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
 public class MainActivity extends AppCompatActivity {
+    LinearLayout llInternet;
+    Button internetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        llInternet = findViewById(R.id.llInternet);
+        internetButton = findViewById(R.id.internetButton);
+
+        checkInternetConnection();
+    }
+
+    private void checkInternetConnection() {
+        if (isOnline()) {
+            llInternet.setVisibility(View.GONE);
+            openFragment();
+        } else {
+            llInternet.setVisibility(View.VISIBLE);
+            internetButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isOnline()) {
+                        openFragment();
+                        llInternet.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
+    }
+
+    private void openFragment() {
         MainFragment mainFragment = new MainFragment();
 
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -45,5 +79,11 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.fragment_container, mainFragment);
 
         transaction.commit();
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 }
